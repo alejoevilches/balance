d48a4bc83c96371f6b1561e342e8befbd80c9fbd
alejoevilches <alejoevilches@gmail.com>
2024-03-04 18:37:24 -0300

    add navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { Navbar } from './components/Navbar'
+import { Notes } from './components/Notes'
 
 function App() {
   return (
     <main>
       <Navbar />
+      <Notes />
     </main>
   )
 }
